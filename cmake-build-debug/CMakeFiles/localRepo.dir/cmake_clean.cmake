8f57074b1d75e49c45325110a661779ea70971c5
Lie <1641024207@qq.com>
2023-03-18 15:55:48 +0800

    无版本文件

---
+++ cmake-build-debug/CMakeFiles/localRepo.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/localRepo.dir/main.c.obj"
+  "liblocalRepo.dll.a"
+  "localRepo.exe"
+  "localRepo.exe.manifest"
+  "localRepo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/localRepo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
